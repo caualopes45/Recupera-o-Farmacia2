c59294969c166f3fd8c0904bfc2343934f3c7d36
caualopes45 <caualopesduarte11@gmail.com>
2026-09-23 14:30:45 -0300

    Coloca o  carregamento dos medicamentos

--- main.py
+++ main.py
@@ -4,4 +4,17 @@ import os
 ARQUIVO_CSV = "medicamentos.csv"
 
 CAMPOS = ["nome", "categoria", "quantidade"]
- 
+def carregar_medicamentos():
+    medicamentos = []
+
+    if not os.path.exists(ARQUIVO_CSV):
+        return medicamentos
+
+    with open(ARQUIVO_CSV, newline="", encoding="utf-8") as arquivo:
+        leitor = csv.DictReader(arquivo)
+
+        for linha in leitor:
+            linha["quantidade"] = int(linha["quantidade"])
+            medicamentos.append(linha)
+
+    return medicamentos
